8cda50cdb379b8ccbc4c5a2d2223af3af87f228c
george <george.benedict@student.moringaschool.com>
2025-06-18 09:34:40 +0300

    commits

---
+++ server/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    pizzas = db.relationship('RestaurantPizza', backref='restaurant')
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String)
+
+    restaurants = db.relationship('RestaurantPizza', backref='pizza')
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
