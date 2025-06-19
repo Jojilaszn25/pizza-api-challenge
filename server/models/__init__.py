646edb73b29963e79556bc686102eeac9dfe92ba
george <george.benedict@student.moringaschool.com>
2025-06-19 09:36:50 +0300

    last commit

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -8,13 +8,12 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    # Configure SQLite DB path relative to the instance folder
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../pizza.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .models import Pizza, Restaurant, RestaurantPizza  # Import your models
+    from .models import Pizza, Restaurant, RestaurantPizza  
 
     return app
